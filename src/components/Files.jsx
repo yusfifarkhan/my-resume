467aa198a7349e049b68f845503f05a9aebbdaf1
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2023-03-21 18:13:51 +0700

    Update CV

--- src/components/Files.jsx
+++ src/components/Files.jsx
@@ -9,7 +9,7 @@ const Files = () => {
     >
       <div className="flex max-w-screen-xl pt-6 pb-2 transition-all lg:px-28">
         <div className=" hidden bg-slate-200 dark:bg-slate-700 lg:inline-flex lg:basis-4/12">
-          <img src="/assets/doc/CV.png" alt="Example CV" />
+          <img src="/assets/doc/CV.jpg" alt="Example CV" />
         </div>
         <div className="w-full rounded-md bg-slate-200 p-4 dark:bg-slate-700 lg:basis-10/12  lg:rounded-none">
           <h1 className="text-lg font-bold dark:text-white lg:text-2xl">
@@ -22,33 +22,30 @@ const Files = () => {
             available:
           </div>
           <br />
-          <div className="grid w-full grid-flow-col place-content-between gap-2 lg:place-content-start">
+          <div className="flex max-w-full gap-2">
             <a
-              href="https://drive.google.com/file/d/1lQuNoL-gsd0DovrFlSK8uGhEgWDkinpe/view?usp=share_link"
+              href="https://drive.google.com/file/d/1-0tTTJEYzK3gyIsQNnmmtXhsdXGh8XHF/view?usp=sharing"
               target="_blank"
+              class="inline-flex items-center rounded bg-primary-500 py-2 px-4 font-bold text-white hover:bg-primary-600"
             >
-              <button className="inline-flex w-fit items-center rounded bg-primary-500 py-2 px-4 text-base font-bold text-white hover:bg-primary-600">
-                <FaLanguage size={"24px"} /> &nbsp;
-                <span>EN Version</span>
-              </button>
+              <FaLanguage size={"24px"} /> &nbsp;
+              <span>EN Version</span>
             </a>
             <a
-              href="https://drive.google.com/file/d/1J-zrrCPfSmTsRH2tBfm09I657dPi0bGv/view?usp=share_link"
+              href="https://drive.google.com/file/d/1-1yrvL1RjnXd2GeNimmi0xz82cdpkxdO/view?usp=sharing"
               target="_blank"
+              class="inline-flex items-center rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-600"
             >
-              <button className="inline-flex w-fit items-center rounded bg-red-500 py-2 px-4 text-base font-bold text-white hover:bg-red-600">
-                <FaLanguage size={"24px"} /> &nbsp;
-                <span>JP Version</span>
-              </button>
+              <FaLanguage size={"24px"} /> &nbsp;
+              <span>JP Version</span>
             </a>
             <a
-              href="https://drive.google.com/file/d/14iObK0niBCIe94MVitAuvmf6UYJV5Zvm/view?usp=share_link"
+              href="https://drive.google.com/file/d/1-3ZV0spD89RYhDQMmbjqpZi0169PDEfx/view?usp=sharing"
               target="_blank"
+              class="inline-flex items-center rounded bg-teal-500 py-2 px-4 font-bold text-white hover:bg-teal-600"
             >
-              <button className="inline-flex w-fit items-center rounded bg-teal-500 py-2 px-4 text-base font-bold text-white hover:bg-teal-600">
-                <FaLanguage size={"24px"} /> &nbsp;
-                <span>JP ATF Version</span>
-              </button>
+              <FaLanguage size={"24px"} /> &nbsp;
+              <span>JP Version ATF</span>
             </a>
           </div>
         </div>
